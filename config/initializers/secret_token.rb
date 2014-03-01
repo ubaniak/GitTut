96d0df67db75d32e02edea1db34d1679aa72a8ce
Bhavek Budhia <bhavek.budhia@gmail.com>
2014-03-01 20:52:51 +0000

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gittut::Application.config.secret_key_base = 'bd4200e81f0c749e9e6323438d112858e640c35ded84aab656e50cd7f4dcca0e848d130c998e3942fe9011a05e457800267b76adb6b5105bf90b2d14df81df54'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gittut::Application.config.session_store :cookie_store, key: '_gittut_session'
